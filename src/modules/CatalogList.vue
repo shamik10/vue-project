f8c79bb84d4d2b9241968b678169f1503d06389c
shamil <shamil.alkhasov04@gmail.com>
2024-04-16 11:13:26 +0300

    commit on laptop

---
+++ src/modules/CatalogList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="flex gap-4">
+    <NameButton 
+      v-for="item in catalog" 
+      :key="item.id"
+      :name="item.name"
+    />
+  </div>
+</template>
+
+<script setup>
+  import axios from 'axios';
+  import NameButton from '@/UI/NameButton.vue';
+  import { onMounted, ref } from 'vue';
+  
+
+  const catalog = ref([])
+
+  const fetchCatalog = async () => {
+    try {
+      const {data} = await axios.get(`https://6d8dc8fcd4ab0089.mokky.dev/catalog`)
+      catalog.value = data
+      console.log(data)
+     
+    }
+    catch(e) {
+      console.log(e)
+    }
+  }
+
+  
+
+  onMounted( async () => {
+    await fetchCatalog()
+  })
+</script>
+
+<style>
+
+</style>
